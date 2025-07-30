fe755041aa2b90a533e3f29448b158a7185978ec
Bard-Gaming <christophe@dronne.fr>
2025-07-30 12:56:50 +0200

    feat: add type checking to utility

--- qtp/utils.py
+++ qtp/utils.py
@@ -9,6 +9,10 @@ def generate_request(*bodies: str) -> dict:
     there will be as many messages within the request as
     given bodies.
     """
+
+    if not all(isinstance(body, str) for body in bodies):
+        raise TypeError("all given bodies must be of type 'str'")
+
     return {
         "Records": [
             {"messageId": str(uuid4()), "body": body}
